e875ac0e32fc9dcef1a5d6a19b43f37d5146106e
SurajV2000 <sv107719@gmail.com>
2022-09-03 19:07:14 +0530

    mathematicals program

---
+++ mathematicalrough/mathproblem2.js
@@ -0,0 +1,19 @@
+let one=1;
+let two=2;
+let three=3;
+let four=4;
+let five=5;
+
+one = 1*2;
+
+two = 2*2;
+
+three = 3*2;
+
+four = 4*2;
+
+five = 5*2;
+
+
+
+console.log(one+two+three+four+five);
---
+++ mathematicalrough/mathproblem3.js
@@ -0,0 +1,22 @@
+let one=1;
+let two=2;
+let three=3;
+let four=4;
+let five=5;
+let six=6;
+
+one = 1;
+
+two = 2*2;
+
+three = 3*3;
+
+four = 4*3;
+
+five = 5*2;
+
+six = 6*2;
+
+
+
+console.log(one+two+three+four+five+six);
